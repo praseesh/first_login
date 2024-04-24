15e4f4318a90005474685c1983e6ac519f8a6c7b
Praseesh <praseeshprasee@gmail.com>
2024-04-24 23:03:31 +0530

    signup page added

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
-from . import views
+from django.urls import path
+from .views import login_user, home, logout_user, sign_up
 
 urlpatterns = [
-    path('',views.login_user),
-    path('login', views.home),
-    path('signout', views.logout_user)
+    path('', login_user, name='login_user'),
+    path('home/', home, name='home'),
+    path('logout_user', logout_user, name='logout_user'),
+    path('signup',sign_up, name="signup" )
 ]
--- login/views.py
+++ login/views.py
@@ -28,9 +28,10 @@
 #     if 'username' in request.session:
 #         request.session.flush()
 #     return redirect(login_user)
-
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
+
 
 def login_user(request):
     if 'username' in request.session:
@@ -58,3 +59,26 @@ def logout_user(request):
     if 'username' in request.session:
         request.session.flush()
     return redirect('login_user')
+
+def sign_up(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        
+        if User.objects.filter(username=username).exists():
+            msg = "Username already taken"
+            return render(request, 'signup.html',{'msg':msg})
+        
+        user = User.objects.create_user(username=username, password=password,email=email)
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            # login(request, user)   
+            return redirect('login_user')
+
+        else:
+            return render(request, 'signup.html', {'error_message': 'Failed to create user.'})
+    
+    return render(request, 'signup.html')
+        
