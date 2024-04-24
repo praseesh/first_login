9e77122e305e9ce1f1d5b7e6233ea4cca23856d0
Praseesh <praseeshprasee@gmail.com>
2024-04-24 13:35:51 +0530

    views changed for session

--- login/urls.py
+++ login/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.login),
-    path('display', views.display),
-    path('submit_btn', views.login)
+    path('',views.login_user),
+    path('login', views.home),
+    path('signout', views.logout_user)
 ]
--- login/views.py
+++ login/views.py
@@ -1,28 +1,60 @@
+# from django.shortcuts import render, redirect
+# from django.contrib.auth import authenticate
+
+    
+# def login_user(request):
+#     if 'username' in request.session:
+#         return redirect(home)
+    
+#     if request.method == "POST":
+#         username = request.POST["username"]
+#         password = request.POST["password"]
+#         user = authenticate(username=username, password=password)
+#         if user is not None:
+#             request.session['username'] = username
+#             return redirect(home)
+#         else:
+#             message =  "Please enter valid credentials"
+#             return render(request,'login.html', {'message':message})
+#     return render(request, 'login.html')
+
+# def home(request):
+#     if 'username' in  request.session:
+#         return render(request, 'home.html')
+#     else:
+#         return redirect(login_user)
+    
+# def logout_user(request):
+#     if 'username' in request.session:
+#         request.session.flush()
+#     return redirect(login_user)
+
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
 
-
-
-
-username = "praseesh"
-password = "pras123"
-def login(request):
-    return render(request, "login.html")
-
-
-
-def display(request):
+def login_user(request):
+    if 'username' in request.session:
+        return redirect('home')
     
-    if request.method == 'POST':
-        uname = request.POST["t1"]
-        pwd = request.POST["t2"]
-        if uname == username and pwd == password:
-            
-            return render(request,"home.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            request.session['username'] = username
+            return redirect('home')
         else:
-            msg = "Invalid Login details"
-            return render(request, "login.html", {"message":msg})
-
-def signout(request):
+            message = "Please enter valid credentials"
+            return render(request, 'login.html', {'message': message})
     return render(request, 'login.html')
+
+def home(request):
+    if 'username' in request.session:
+        return render(request, 'home.html')
+    else:
+        return redirect('login_user')
     
-    
+def logout_user(request):
+    if 'username' in request.session:
+        request.session.flush()
+    return redirect('login_user')
