ae0b8ae957b96b8f6bc5990e7e135a93189a3499
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-05-27 10:27:47 +0530

    Update urls.py

--- login/urls.py
+++ login/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', login_user, name='login_user'),
     path('home', home, name='home'),
     path('logout_user', logout_user, name='logout_user'),
-    path('signup',sign_up, name="signup" )
+    path('signup',sign_up, name="signup" ),
 ]
