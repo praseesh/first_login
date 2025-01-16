421a26f5ba53fa12d0ac085ed071b3ae61aaea45
Praseesh <150511158+praseesh@users.noreply.github.com>
2025-01-16 23:14:33 +0530

    Update settings.py

--- loginpage/settings.py
+++ loginpage/settings.py
@@ -6,9 +6,6 @@ from datetime import timedelta
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=1_pak6)$$@79)1(oodbz8b654tqfj)zfj+3yy!4obnz0v0_%q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
